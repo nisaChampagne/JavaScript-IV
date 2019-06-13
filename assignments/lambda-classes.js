0fda453b94a4d5635b7563e6371f39cfe2e85eaf
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-06-13 14:59:57 -0400

    Almost done

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -18,5 +18,70 @@ class student extends Person{
     }
     listSubject(student){
         console.log(`${student.favSubject[0]}, ${student.favSubject[1]}, ${student.favSubject[3]} `)
-    }
+    };
+    PRAssignment(subject){
+        console.log(`${student.name} has submitted a PR for ${subject}`)
+    };
+    sprintChallenge(subject){
+        console.log(`${student.name} has begun sprint challenge on ${subject}`)
+    };
 }
+class Instructor extends Person{
+    constructor(attributes){
+        super(attributes);
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+    demo(subject){
+        console.log(`Today we are learning about ${subject}`);
+    };
+    grade(student, subject){
+        console.log(`${student.name} receives a perfect score on ${subject}`)
+    };
+}
+
+class ProjectManager extends Instructor{
+    constructor(attributes){
+        super(attributes);
+        this.gradClassName = attributes.gradClassName;
+        this.favInstructor = attributes.favInstructor;
+    }
+    standUp(channel){
+        console.log(`${this.name} announces to ${channel}, @channel standy times!`)
+    };
+    debugsCode(studentObj, subject){
+        console.log(`${this.name} debugs ${studentObj.name}'s code on ${subject}`)
+    };
+}
+
+
+
+const nisa = new student({
+    name: 'Nisa',
+    age: 25,
+    location: 'Ohio',
+    previousBackground: 'Debt Collector',
+    className: 'Web21',
+    favSubjects: ['Html', 'CSS', 'JavaScript'],
+});
+
+const dan = new Instructor({
+    name: 'Dan',
+    age: 'Infinity',
+    location: 'Denver',
+    specialty: 'Relentless Debugger',
+    favLanguage: 'JavaScript, Python, Elm, ReasonML, SmallTalk, Haskell, C#, Java, Rust, Go, Ruby, Crystal, Elixir, Lua, and Julia',
+    catchPhrase: 'If you can do the thing, you can get paid to do the thing!',
+});
+
+const mary = new ProjectManager({
+    name: 'Mary',
+    age: '24',
+    gradClassName: 'WEB18',
+    favInstructor: 'Josh Knell',
+    location: 'New York',
+    specialty: 'Express and Node.js',
+    favLanguage: 'Javascript',
+    catchPhrase: "That looks AWESOME",
+});
